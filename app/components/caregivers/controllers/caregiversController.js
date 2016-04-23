6772e79a898d56d440ee209626686cdb1ce9adde
José Cortez <Jospcortez@gmail.com>
2016-04-23 21:09:41 +0100

    Basic routing is done

---
+++ app/components/caregivers/controllers/caregiversController.js
@@ -0,0 +1,16 @@
+angular.module("CareFull").controller("caregiversController", ['$scope', function($scope) {
+   $scope.caregivers = [
+      {
+         name: "João Vilela",
+         rating: 4
+      },
+      {
+         name: "João Vilela",
+         rating: 4
+      },
+      {
+         name: "João Vilela",
+         rating: 4
+      }
+   ]
+}]);
--- app/routes.js
+++ app/routes.js
@@ -7,9 +7,21 @@ angular.module("CareFull").config(['$stateProvider', '$urlRouterProvider', funct
      url :'/',
      templateUrl: 'app/components/home/views/homepage.html'
  })
-  .state('caregivers', {
-    url: '/caregivers',
-    templateUrl: 'app/components/caregivers/views/caregivers.html'
+  .state('seeker', {
+    url: '/1',
+    templateUrl: 'app/components/caregivers/views/seeker-maincontent.html'
+   })
+   .state('seeker.caregivers' ,{
+      url: '/caregivers',
+      templateUrl: 'app/components/caregivers/views/caregivers.html'
+   })
+   .state('seeker.profile' ,{
+      url: '/profile',
+      templateUrl: 'app/components/caregivers/views/profile.html'
+   })
+   .state('seeker.messages' ,{
+      url: '/messages',
+      templateUrl: 'app/components/caregivers/views/messages.html'
    });
 
 
