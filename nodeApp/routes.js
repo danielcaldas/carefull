f4e35b2e86c1c57d9b7d6c76bd667afe667b9051
José Cortez <Jospcortez@gmail.com>
2016-04-23 22:25:08 +0100

    MEAN pt.2: Slimmest back-end ever!

---
+++ nodeApp/routes.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+//var mongoose        = require('mongoose');
+
+module.exports = function(app) {
+
+  //Get the book's info through the ISBN
+  app.get("/:caregiver", function (req,res) {
+     console.log("woo!")
+  });
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+// -----------------------------------------------------
+var express         = require('express');
+var mongoose        = require('mongoose');
+var port            = process.env.PORT || 3000;
+var morgan          = require('morgan');
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+var app             = express();
+
+
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
+app.use(morgan('dev'));                                         // log with Morgan
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
+app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// Routes
+// ------------------------------------------------------
+require('./nodeApp/routes.js')(app);
+
+// Listen
+// -------------------------------------------------------
+app.listen(port);
+console.log('App listening on port ' + port);
