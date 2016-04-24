29ee2cbbc7d1f0acc0a62b3ed11c077c407ad449
José Cortez <Jospcortez@gmail.com>
2016-04-24 01:25:12 +0100

    Add the 'Load More' button

--- public/app/components/caregivers/controllers/caregiversController.js
+++ public/app/components/caregivers/controllers/caregiversController.js
@@ -1,4 +1,8 @@
 angular.module("CareFull").controller("CaregiversController", ['$scope', function($scope) {
+
+   $scope.limit = 3;
+
+
    $scope.caregivers = [
       {
          name: "João Vilela",
@@ -20,6 +24,72 @@ angular.module("CareFull").controller("CaregiversController", ['$scope', functio
          price: 1.1,
          rating: 2,
          occupation: "Estudante"
+      },{
+         name: "João Vilela",
+         city: "Braga",
+         price: 3.5,
+         rating: 4,
+         occupation: "Fisioterapeuta"
+      },
+      {
+         name: "Miguel Chouriço",
+         city: "Porto",
+         price: 7.6,
+         rating: 3,
+         occupation: "Enfermeiro"
+      },
+      {
+         name: "Francisco Rolando",
+         city: "Lisboa",
+         price: 1.1,
+         rating: 2,
+         occupation: "Estudante"
+      },{
+         name: "João Vilela",
+         city: "Braga",
+         price: 3.5,
+         rating: 4,
+         occupation: "Fisioterapeuta"
+      },
+      {
+         name: "Miguel Chouriço",
+         city: "Porto",
+         price: 7.6,
+         rating: 3,
+         occupation: "Enfermeiro"
+      },
+      {
+         name: "Francisco Rolando",
+         city: "Lisboa",
+         price: 1.1,
+         rating: 2,
+         occupation: "Estudante"
+      },{
+         name: "João Vilela",
+         city: "Braga",
+         price: 3.5,
+         rating: 4,
+         occupation: "Fisioterapeuta"
+      },
+      {
+         name: "Miguel Chouriço",
+         city: "Porto",
+         price: 7.6,
+         rating: 3,
+         occupation: "Enfermeiro"
+      },
+      {
+         name: "Francisco Rolando",
+         city: "Lisboa",
+         price: 1.1,
+         rating: 2,
+         occupation: "Estudante"
       }
-   ]
+   ];
+
+   $scope.loadMore = function() {
+      console.log($scope.caregivers.length);
+      var incremented = $scope.limit + 3;
+      $scope.limit = incremented > $scope.caregivers.length ? $scope.caregivers.length : incremented;
+    };
 }]);
