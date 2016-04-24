ed2841a4c5974510da4e7e692a9008ec989f825f
José Cortez <Jospcortez@gmail.com>
2016-04-24 04:52:53 +0100

    VERY BIG DATA!!

--- public/app/components/caregivers/controllers/CaregiverProfileController.js
+++ public/app/components/caregivers/controllers/CaregiverProfileController.js
@@ -1,5 +1,15 @@
 angular.module("CareFull").controller("CaregiverProfileController", ['$scope','Caregiver', function($scope, Caregiver) {
    $scope.caregiver = Caregiver;
 
+   $scope.getSkills = function (){
+      var i;
+      var str = "";
+      var skills = $scope.caregiver.skills;
+      for (i=0;i<skills.length-1;i++)
+         str += skills[i][1] + ", ";
+      str += skills[i][1];
+      console.log(str);
+      return str;
+   }
 
 }]);
