1d5e86919cbd45bd49f6579bfc758f194858d7f5
danielcaldas <danielcaldas@sapo.pt>
2016-04-24 09:02:49 +0100

    Search form

--- public/app/routes.js
+++ public/app/routes.js
@@ -1,49 +1,36 @@
 angular.module("CareFull").config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-                $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/');
 
 
-                $stateProvider
-                    .state('homepage', {
-                        url: '/',
-                        templateUrl: 'app/components/home/views/homepage.html'
-                    })
-                    .state('seeker', {
-                        url: '/caregivers',
-                        templateUrl: 'app/components/caregivers/views/seeker-maincontent.html',
-                        controller: 'CaregiversController'
-                    })
-                    .state('seeker.caregivers', {
-                        url: '/caregivers',
-                        views: {
-                            'searchform': {
-                                templateUrl: 'app/components/caregivers/views/filters.html',
-                                controller: 'CaregiversController'
-                            },
-                            'caregivers': {
-                                templateUrl: 'app/components/caregivers/views/caregivers.html',
-                                controller: 'CaregiversController'
-                            }
-                           }
-                        })
-                        .state('seeker.profile', {
-                            url: '/profile',
-                            templateUrl: 'app/components/caregivers/views/profile.html'
-                        })
-                        .state('seeker.messages', {
-                            url: '/messages',
-                            templateUrl: 'app/components/caregivers/views/messages.html'
-                        })
-                        .state('seeker.caregiverProfile', {
-                            url: '/:caregiverID',
-                            templateUrl: 'app/components/caregivers/views/caregiver-profile.html',
-                            controller: 'CaregiverProfileController',
-                            resolve: {
-                                Caregiver: ['CaregiverService', function(CaregiverService) {
-                                    return CaregiverService.getCaregiver();
-                                }]
-                            }
+  $stateProvider
+  .state('homepage', {
+    url: '/',
+    templateUrl: 'app/components/home/views/homepage.html'
+  })
+  .state('seeker', {
+    url: '/caregivers',
+    templateUrl: 'app/components/caregivers/views/seeker-maincontent.html',
+    controller: 'CaregiversController'
+  })
+  .state('seeker.profile', {
+    url: '/profile',
+    templateUrl: 'app/components/caregivers/views/profile.html'
+  })
+  .state('seeker.messages', {
+    url: '/messages',
+    templateUrl: 'app/components/caregivers/views/messages.html'
+  })
+  .state('seeker.caregiverProfile', {
+    url: '/:caregiverID',
+    templateUrl: 'app/components/caregivers/views/caregiver-profile.html',
+    controller: 'CaregiverProfileController',
+    resolve: {
+      Caregiver: ['CaregiverService', function(CaregiverService) {
+        return CaregiverService.getCaregiver();
+      }]
+    }
 
-                        });
+  });
 
 
-                    }]);
+}]);
